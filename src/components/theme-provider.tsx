db83ad78475816d733c34753d8208985b10d6505
guilherme6191 <guilherme@clerk.dev>
2025-08-03 18:05:13 -0300

    fix: Fix ts issue

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -2,12 +2,12 @@
 
 import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { ComponentProps } from "react";
 
-export function ThemeProvider({
-  children,
-  ...props
-}: {
+type ThemeProviderProps = {
   children: React.ReactNode;
-}) {
+} & ComponentProps<typeof NextThemesProvider>;
+
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
